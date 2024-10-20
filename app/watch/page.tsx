f92e48dbab43c37045ae993a88aba7dacc4a4cc9
Savar Bhasin <savarbhasin446@gmail.com>
2024-10-20 16:25:46 +0530

    wrap usesearhc params

--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -4,34 +4,27 @@ import { VideoChatSummary } from '@/components/VideoSummary';
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
-const Fallback = () =>{
+
+const VidComponent = ()=>{
+  const searchParams = useSearchParams()
+ 
+  const videoId = searchParams.get('v');
+
   return (
-    <div>
-            <h1 className="text-3xl font-semibold text-gray-900">No video found</h1>
-            <p className="text-gray-700">Please enter the correct query parameters.</p>
-      </div>
+    <>
+      {videoId === null ? <div>Video not found</div> : <VideoChatSummary videoId={videoId} />}
+    </>
   )
 }
 
 const Page = () => {
-  const searchParams = useSearchParams()
- 
-  const videoId = searchParams.get('v');
+  
   return (
-    <Suspense fallback={<Fallback/>}>
+    <Suspense>
       
-      {
-        videoId ? (
-          <div>
-            <VideoChatSummary videoId={videoId} />
-          </div>
-        ) : (
-          <div>
-            <h1 className="text-3xl font-semibold text-gray-900">No video found</h1>
-            <p className="text-gray-700">Please enter the correct query parameters.</p>
-          </div>
-        )
-      }
+      <VidComponent />
+          
+        
     </Suspense>
     
   );
